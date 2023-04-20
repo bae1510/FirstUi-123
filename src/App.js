b589102acb59a5c7f19718cd0b6f43ecc947e372
batman <batman@gmai.com>
2023-04-20 18:48:19 +0530

    add new file in branch1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
+  
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img src="img/logo192.png" alt="image" />
+      <h1>Benifits of using react</h1>
+
+           <p>1. Component Based Architecture</p>
+           <p>2. Virtual DOM for efficient updates</p>
+           <p>3. Rich ecosystem and community</p>
+           <p>4. cross-platform development</p>
+           <p>5. strong community support:</p>
+
+            <button>
+              get started ...
+            </button>
+
     </div>
+
+
   );
 }
 
