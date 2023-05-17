77d778db6a851a75ef6b861af6031f10a0adccc7
bae151 <priyalsoni46@gmail.com>
2023-05-17 12:00:13 +0530

    this is routing first assignment

--- src/App.js
+++ src/App.js
@@ -2,12 +2,30 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './components/molecules/header.js';
 import Section from './components/molecules/section.js';
+import Home from './components/pages/Home/home';
+import About from './components/pages/About/about';
+import Project from './components/pages/Project/Project';
+import Contact from './components/pages/Contact/Contact';
+import Video from './components/pages/Video/Video';
+import {Routes, Route, } from "react-router-dom";
+import { Link } from 'react-router-dom';
+
+
 
 function App() {
+  
   return (
     <div className="App">
-      <Header/>
+      <Header />
       <Section/>
+      <Routes>
+        <Route path="/home" element={<Home/>}/>
+        <Route path="/about" element={<About/>} />
+         <Route path = "/project"  element={<Project/>}/>
+       {/* <Route path = "/video"  element={<Video/>}/>
+        <Route path = "/contact"  element={<Contact/>}/>
+         */}
+      </Routes>
     </div>
   );
 }
--- src/components/molecules/header.js
+++ src/components/molecules/header.js
@@ -2,17 +2,52 @@ import React from 'react'
 import styles from './header.module.css';
 //import {ImSphere} from 'react-icons/im'
 //ImSphere
+// BiMenu
+import {BiMenu} from 'react-icons/bi'
+// import {  useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
-function header() {
+function Header() {
+  // const navigate =useNavigate()
+
+
+  // function handleClickHome(){
+  //   navigate('/home')
+  // }
+
+  // function handleClickAbout(){
+  //   navigate('/about')
+  // }
 
    const navOptions = [
-        'Home',
-        'About' ,
-        'Project' ,
-        'Videos' ,
-        'Contact' 
-        
+    // {
+    //     home:'/home',
+    //     about:'/about' ,
+    //     project:'/Project' ,
+    //     video:'/Videos' ,
+    //     Contact:'/Contact' 
+    //  }
+
+    // <button onClick={handleClickHome}>Home</button>,
+    // <button onClick={handleClickAbout}>about</button>,
+    // <button onClick={handleClickProject}>Project</button>,
+    // <button onClick={handleClickVideo}>Video</button>
+
+    <Link to="/home" >Home</Link>,
+              <Link to="/about" >About </Link>,
+              <Link to='/services' >Project</Link>,
+              <Link to="/terms" >Video</Link>,
+              <Link to="/contact" >Contact</Link>,
+             
     ]
+
+    
+    // const handleClick = function (e) {
+    //    // console.log(e)
+    //    const pathToRedirect = navOptions[e]
+    //    navigate(pathToRedirect);
+
+    // }
   return (
     <header>
         <div  className={styles.navOptions}>
@@ -20,14 +55,16 @@ function header() {
         <img  className={styles.icon} src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS-X4ppRWmpB1bwnpjS3jgQysBW-znbhFjNKA&usqp=CAU"/>
             {
                 navOptions.map(page =>
-                (<p>{page}</p>))
+                (<p >{page}</p>))
             }
             <button className={styles.btn}>Login</button>
+            <BiMenu className={styles.menu}/>
         </div>
+
        
     </header>
     
   )
 }
 
-export default header
+export default Header
